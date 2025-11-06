2708496ae80e03a99acf96b62de4b5e11f260d90
kit <kit@kits-MacBook-Air.local>
2025-11-06 09:51:10 +0700

    -

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,7 +43,7 @@ def health_check() -> dict:
         Health status of the API.
     """
     return {
-        "status": "healthy kit 555666",
+        "status": "healthy kit 555666777",
         "service": settings.PROJECT_NAME,
         "version": settings.API_VERSION
     }
